4dd02ab31bc7f429fb2ddb201a6b5608c6e5625b
KiurArbeiter <127192996+KiurArbeiter@users.noreply.github.com>
2023-10-03 09:12:32 +0300

    Create Index.php
    Jänes

---
+++ Index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $firstname = "John <br>";
+    $lastname = "Smith <br>";
+    $fullname = $firstname." ".$lastname;
+    $zodiac = "zodiac sign<br>";
+    $birthdate = "01.01.2000 <br>";
+
+    echo $fullname;
+    echo $birthdate;
+    echo $zodiac;
+    echo '“It’s My Life” – Dr. Alban<br>';
+
+    echo "(\(\\<br>";
+    echo "( -.-)<br>";
+    echo 'o_(")(")<br>';
+
+
+    ?>
+</body>
+</html>
